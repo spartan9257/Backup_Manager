5366b1bc4c92ccc94f4123bfceb31187b36dba99
Samuel Bravo <spartan9257@gmail.com>
2021-02-15 12:17:13 -0600

    -Added print statements to narrate the script -fixed an issue that caused the main backups folder to not update it's last modified time -Some other stuff, idk

--- backup_manager.py
+++ backup_manager.py
@@ -10,28 +10,55 @@ TODO: Compress the backups
     tar -cf uncompressed compressed.gzip #Compress dir
     tar -xvf compressed.gzip             #Uncompress dir
 '''
+#"C:\\Users\\cftbr\\Desktop\\test_folder\\"
+max_backups = 13
+backups_dir = "B:\\DC-Backup\\" 
+backup_folder_name = "WindowsImageBackup"
 
-max_backups = 12
-backups_dir = "backup_manager\\"
-
+#Create and remove a temp file to update the original backup's parent folder last modified time
+tmp_file = backups_dir + backup_folder_name + "\\tmp.txt"
+print("Updating " + backup_folder_name + "'s last modified time", end='')
+try:
+    file = open(tmp_file, "x")
+    file.close()
+    os.system("del " + tmp_file)
+except:
+    os.system("del " + tmp_file)
+print("...COMPLETE")
+    
 #Switch to the backups directory and get a list of all the backups in newest to oldest order.
-backup_list = check_output("dir /OD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
+print("Getting list of backups", end="")
+backup_list = check_output("dir /OD /AD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
 backup_list.pop()
+print("...COMPLETE")
+print("\nBackups found: ")
+for name in backup_list:
+    print(name)
+print("")
 
-#Get the current date and use it to create a new folder that will be used to store the copy 
-#of the most recent backup.
+#Use date to create archive folder for the backup
 current_date = check_output("date /T", shell=True).decode()
 folder_name = str(current_date[4:current_date.find("\n") -2 ]).replace("/","-")
+print("Creating backup archive folder", end='')
 os.system("mkdir " + backups_dir + folder_name)
+print("...COMPLETE")
 
 #Create the new backup copy and set it to ready only
+print("Creating backup", end='')
 os.system("xcopy /E /I /H /V /K /Y " + backups_dir + '"' + backup_list[-1] + '" ' + backups_dir + folder_name)
+print("...COMPLETE")
+print("Setting all sub-directories and files to read-only", end='')
 os.system("attrib /S /D +R " + backups_dir + backup_list[-1] + "\\*") 
+print("...COMPLETE")
 
-#Update the backups list with to include the newly created backup. If the backups exceed a 
-#predefined threshhold, delete the oldest one.
+#Update the backups list with with the new backup. If the backups exceed max limit, delete the oldest one.
+print("Updating the backups list", end='')
 backup_list = check_output("dir /OD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
 backup_list.pop()
+print("...COMPLETE")
 if len(backup_list) >= max_backups and backup_list[0] != "WindowsImageBackup":
+    #delete_count = len(backup_list) - max_backups
+    print("Maximum backups exceeded, deleting the oldest archive [" + backup_list[0] + "]", end='')
     os.system("rmdir /S /Q " + backups_dir + backup_list[0])
+    print("...COMPLETE")
     
