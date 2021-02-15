234cc787aadc8c1f26cdfbbebf3c57bcf2b430c4
Samuel Bravo <spartan9257@gmail.com>
2021-02-15 14:08:37 -0600

    -Added some exception handling -Added the ability to compress the copy of the backup

--- backup_manager.py
+++ backup_manager.py
@@ -10,25 +10,77 @@ TODO: Compress the backups
     tar -cf uncompressed compressed.gzip #Compress dir
     tar -xvf compressed.gzip             #Uncompress dir
 '''
-#"C:\\Users\\cftbr\\Desktop\\test_folder\\"
-max_backups = 13
-backups_dir = "B:\\DC-Backup\\" 
-backup_folder_name = "WindowsImageBackup"
+
+max_backups = 4
+backups_dir = "C:\\Users\\cftbr\\Desktop\\test_folder\\"
+backup_new_backup_name = "WindowsImageBackup"
 
 #Create and remove a temp file to update the original backup's parent folder last modified time
-tmp_file = backups_dir + backup_folder_name + "\\tmp.txt"
-print("Updating " + backup_folder_name + "'s last modified time", end='')
 try:
+    tmp_file = backups_dir + backup_new_backup_name + "\\tmp.txt"
+    print("Updating " + backup_new_backup_name + "'s last modified time", end='')
     file = open(tmp_file, "x")
     file.close()
     os.system("del " + tmp_file)
+    print("...COMPLETE")
 except:
     os.system("del " + tmp_file)
-print("...COMPLETE")
     
 #Switch to the backups directory and get a list of all the backups in newest to oldest order.
-print("Getting list of backups", end="")
-backup_list = check_output("dir /OD /AD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
+try:
+    print("Getting list of backups", end="")
+    backup_list = check_output("dir /OD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
+    backup_list.pop()
+    print("...COMPLETE")
+    
+    print("\nBackups found: ")
+    for name in backup_list:
+        print(name)
+    print("")
+except:
+    print("ERROR: Unable to get list of backups!")
+
+#Use date to create archive folder for the backup
+try:
+    current_date = check_output("date /T", shell=True).decode()
+    new_backup_name = backups_dir + str(current_date[4:current_date.find("\n") -2 ]).replace("/","-")
+    print("Creating new backup folder", end='')
+    os.system("mkdir " + new_backup_name)
+    print("...COMPLETE")
+except:
+    print("ERROR: Unable to create new backup folder!")
+
+#Create the new backup copy and set it to ready only
+try:
+    print("Creating backup", end='')
+    os.system("xcopy /E /I /H /V /K /Y " + backups_dir + '"' + backup_list[-1] + '" ' + new_backup_name)
+    print("...COMPLETE")
+    
+    print("Setting all sub-directories and files to read-only", end='')
+    os.system("attrib /S /D +R " + backups_dir + backup_list[-1] + "\\*") 
+    print("...COMPLETE")
+except:
+    print("ERROR: Unable to create backup!")
+ 
+#Create a compressed copy of the new backup 
+try:
+    print("Compressing backup...", end="")
+    os.system("tar -czvf " + new_backup_name + ".tar.gz " + new_backup_name)
+    print("...COMPLETE")
+    
+    print("Removing uncompressed backup...", end="")
+    os.system("rmdir /S /Q " + new_backup_name)
+    print("...COMPLETE")
+    
+    print("Setting compressed backup to read-only", end='')
+    os.system("attrib /S /D +R " + new_backup_name + ".tar.gz")
+    print("...COMPLETE")
+except:
+    print("ERROR: compression unsuccessful!")
+
+#Update the backups list with with the new backup
+print("Updating the backups list", end='')
+backup_list = check_output("dir /OD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
 backup_list.pop()
 print("...COMPLETE")
 print("\nBackups found: ")
@@ -36,29 +88,9 @@ for name in backup_list:
     print(name)
 print("")
 
-#Use date to create archive folder for the backup
-current_date = check_output("date /T", shell=True).decode()
-folder_name = str(current_date[4:current_date.find("\n") -2 ]).replace("/","-")
-print("Creating backup archive folder", end='')
-os.system("mkdir " + backups_dir + folder_name)
-print("...COMPLETE")
-
-#Create the new backup copy and set it to ready only
-print("Creating backup", end='')
-os.system("xcopy /E /I /H /V /K /Y " + backups_dir + '"' + backup_list[-1] + '" ' + backups_dir + folder_name)
-print("...COMPLETE")
-print("Setting all sub-directories and files to read-only", end='')
-os.system("attrib /S /D +R " + backups_dir + backup_list[-1] + "\\*") 
-print("...COMPLETE")
-
-#Update the backups list with with the new backup. If the backups exceed max limit, delete the oldest one.
-print("Updating the backups list", end='')
-backup_list = check_output("dir /OD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
-backup_list.pop()
-print("...COMPLETE")
+#If the backups exceed max limit, delete the oldest one
 if len(backup_list) >= max_backups and backup_list[0] != "WindowsImageBackup":
-    #delete_count = len(backup_list) - max_backups
     print("Maximum backups exceeded, deleting the oldest archive [" + backup_list[0] + "]", end='')
-    os.system("rmdir /S /Q " + backups_dir + backup_list[0])
+    os.system("del /S /Q " + backups_dir + backup_list[0])
     print("...COMPLETE")
     
