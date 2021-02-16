56586713e44e87abe0de4bb076a082f623ae43ca
Samuel Bravo <spartan9257@gmail.com>
2021-02-16 11:01:42 -0600

    -Changed the backup process so that rather than: creating a copy of the backup, then creating a compressed copy of the copy, then deleting the original copy. Now a compressed copy is created from the beginning, significantly improving process time and reducing the amount of extra storages that's needed for the headspace of hosting the multiple copies temporarily.
    -Updated the backup removal process so that it removes the read-only property from the backup before removal. The deletion of a compressed file set to read only requires elevated privileges.

--- backup_manager.py
+++ backup_manager.py
@@ -1,5 +1,6 @@
 import os
 from subprocess import check_output
+from os import path
 
 '''
 The purpose of this script is to automate the management of windows backups on a local drive. The script will
@@ -15,7 +16,7 @@ max_backups = 4
 backups_dir = "C:\\Users\\cftbr\\Desktop\\test_folder\\"
 backup_folder_name = "WindowsImageBackup"
 
-#Create and remove a temp file to update the original backup's parent folder last modified time
+#Create and remove a temp file to update the backup's [parent folder] last modified time
 try:
     tmp_file = backups_dir + backup_folder_name + "\\tmp.txt"
     print("Updating " + backup_folder_name + "'s last modified time", end='')
@@ -26,13 +27,12 @@ try:
 except:
     os.system("del " + tmp_file)
     
-#Switch to the backups directory and get a list of all the backups in newest to oldest order.
+#Get a list of all the archived backups (old to new)
 try:
     print("Getting list of backups", end="")
     backup_list = check_output("dir /OD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
     backup_list.pop()
     print("...COMPLETE")
-    
     print("\nBackups found: ")
     for name in backup_list:
         print(name)
@@ -40,47 +40,22 @@ try:
 except:
     print("ERROR: Unable to get list of backups!")
 
-#Use date to create archive folder for the backup
+#Create the archived backup
 try:
+    #Get the date and format it MM-DD-YYYY
     current_date = check_output("date /T", shell=True).decode()
     new_backup_name = backups_dir + str(current_date[4:current_date.find("\n") -2 ]).replace("/","-")
-    print("Creating new backup folder", end='')
-    #os.system("mkdir " + new_backup_name)
-    print("...COMPLETE")
-except:
-    print("ERROR: Unable to create new backup folder!")
-
-#Create the new backup copy and set it to ready only
-'''
-try:
-    print("Creating backup", end='')
-    os.system("xcopy /E /I /H /V /K /Y " + backups_dir + '"' + backup_list[-1] + '" ' + new_backup_name)
-    print("...COMPLETE")
     
-    print("Setting all sub-directories and files to read-only", end='')
-    os.system("attrib /S /D +R " + backups_dir + backup_list[-1] + "\\*") 
-    print("...COMPLETE")
-except:
-    print("ERROR: Unable to create backup!")
-'''
- 
-#Create a compressed copy of the new backup 
-try:
-    print("Compressing backup...", end="")
+    #Create and compress archived copy, set it to read-only
+    print("Creating backup...", end="")
     os.system("tar -czvf " + new_backup_name + ".tar.gz " + backups_dir + backup_list[-1])
-    print("...COMPLETE")
-    '''
-    print("Removing uncompressed backup...", end="")
-    os.system("rmdir /S /Q " + new_backup_name)
-    print("...COMPLETE")
-    '''
-    print("Setting compressed backup to read-only", end='')
     os.system("attrib /S /D +R " + new_backup_name + ".tar.gz")
     print("...COMPLETE")
 except:
-    print("ERROR: compression unsuccessful!")
+    print("ERROR: Unable to create backup!")
+ 
 
-#Update the backups list with with the new backup
+#Update the backups list to include the new archived backup (old to new)
 print("Updating the backups list", end='')
 backup_list = check_output("dir /OD /B " + backups_dir, shell=True).decode().replace("\r", "", 999).split("\n")
 backup_list.pop()
@@ -92,7 +67,11 @@ print("")
 
 #If the backups exceed max limit, delete the oldest one
 if len(backup_list) >= max_backups and backup_list[0] != "WindowsImageBackup":
-    print("Maximum backups exceeded, deleting the oldest archive [" + backup_list[0] + "]", end='')
+    print("Maximum backups exceeded, deleting the oldest archive [" + backup_list[0] + "]", end="")
+    os.system("attrib -R " + backups_dir + backup_list[0])
     os.system("del /S /Q " + backups_dir + backup_list[0])
-    print("...COMPLETE")
+    if path.exists(backups_dir + backup_list[0]):
+        print("\nERROR: Unable to remove archive!")
+    else:
+        print("...COMPLETE")
     
